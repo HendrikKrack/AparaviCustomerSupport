45ea65dd8cd7541834823acf0878999ce2d780ca
Hendrik Krack <hendrik.krack@aparavi.com>
2025-01-20 23:51:29 -0800

    latest and greatest changes

--- UserInterface.py
+++ UserInterface.py
@@ -1,10 +1,6 @@
 from openai import OpenAI
 import streamlit as st
-import os
-import shelve
-import json
 from PIL import Image
-
 from qdrant_client import QdrantClient, models
 
 # need to install openai dotenv streamlit
@@ -15,79 +11,39 @@ qdrant_client = QdrantClient(
 )
 
 #images
-logoHult    = Image.open("images/headLogoAparavi.png")
-nAibleLogo  = Image.open("images/nAibleLogo.png")
-AparaviLogo = Image.open("images/logoAparaviDarkMode.png")
+logoHult = Image.open("images/headLogoAparavi.png")
 st.image(logoHult)
 
 
 
-#st.title("AI Tech Ambassodor👩‍💻🛠") 
-BOT_AVATAR = "images/iconhult2.png" #S"👩‍💻🦾"  
-st.markdown("<h1 style='text-align: center;'>Tech Ambassador AI Agent</h1>", unsafe_allow_html=True) 
-##################################################
-################################################## a9e7907
+BOT_AVATAR = "images/aparaviLogoIcon.jpg"  
+st.markdown("<h1 style='text-align: center;'>Aparavi Customer Support Agent</h1>", unsafe_allow_html=True) 
 
 # initialize the retriever and the embedding model
-from openai import OpenAI
-
-
 OPENAI_API_KEY = "your-openai-api-key"
-
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-response = client.embeddings.create(
-    input="Your text string goes here",
-    model="text-embedding-3-small"
-)
-
-# Ensure openai_model is initialized in session state
+# Initialize session state variables
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4o"
 
-
-# Load chat history from shelve file
-def load_chat_history():
-    with shelve.open("chat_history") as db:
-        return db.get("messages", [])
-
-
-# Save chat history to shelve file
-def save_chat_history(messages):
-    with shelve.open("chat_history") as db:
-        db["messages"] = messages
-
-
 if "messages" not in st.session_state:
-    st.session_state.messages = load_chat_history()
-
+    st.session_state.messages = []
 
 # Initialize the sidebar
 with st.sidebar:
     st.write("If you were able to resolve an issue and wish to continue with another topic, you should...")
 
-    # Button to delete chat history
-    if st.button("Delete Chat History"):
+    # Button to clear current chat
+    if st.button("Clear Chat"):
         st.session_state.messages = []
-        save_chat_history([])
-
-    st.write("Designed by")
-
-    # Insert the nAible logo
-    st.image(nAibleLogo)  
-
-    st.write("Powered by")
-
-    # Insert the Aparavi logo
-    st.image(AparaviLogo)
-
 
 # Password protection
 def check_password():
     """Returns `True` if the user had the correct password."""
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == "HultTech2024":
+        if st.session_state["password"] == "ask-aparavi-ai":
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # Don't store the password
         else:
@@ -96,7 +52,7 @@ def check_password():
 
     # First run, show input for password
     st.text_input(
-        "Please enter the password to access the Tech Ambassador", 
+        "Please enter the password to access the Aparavi Support Agent", 
         type="password", 
         on_change=password_entered, 
         key="password"
@@ -111,12 +67,12 @@ def check_password():
 if check_password():
     # Display chat messages
     for message in st.session_state.messages:
-        avatar = USER_AVATAR if message["role"] == "user" else BOT_AVATAR
+        avatar = "👩‍💻" if message["role"] == "user" else BOT_AVATAR
         with st.chat_message(message["role"], avatar=avatar):
             st.markdown(message["content"])
 
     # Main chat interface
-    if prompt := st.chat_input("Hi there! I am your virtual Hult Tech Ambassador. How can I help?"):
+    if prompt := st.chat_input("Hi there! I am your virtual Aparavi assistant. How can I help?"):
         
         # Get the vectors    
         response = client.embeddings.create(
@@ -128,13 +84,11 @@ if check_password():
 
         # perform semantic search 
         semanticResponse = qdrant_client.search(
-            collection_name="HultTechSupportV3", query_vector=queryVectors    , limit=5
+            collection_name="AparaviChatbot", query_vector=queryVectors, limit=5
         )
 
-        #print(semanticResponse)
         # augment the prompt
         augmentedPrompt = f"""
-
             You are an AI assistant for Hult Tech Ambassadors. A user has asked the following question: "{prompt}".
 
             Your task is to:
@@ -148,7 +102,6 @@ if check_password():
             - If you retrieve a document with a cosine similarity above 0.5, ignore it.
             - If you retrieve multiple relevant results, choose the most relevant 1-3 results and integrate the information into your response.
 
-            
             The format of your response should be:
             - Provide useful context that answers the user's question.
             - If the user asks you to support them in creating a ticket, answer them with the following the form that is provided on the website: "Which Team is your query for?" -> Hult Technology, "Service Category" -> (You have to decide beased on the user input and context),
@@ -156,7 +109,6 @@ if check_password():
             then send them to this link: https://ef-hult.freshservice.com/support/tickets/new
             
             If there are no relevant search results, provide a helpful general response or ask a follow-up question to clarify what the user is looking for.
-
 
             and here is some details I have retrieved through semantic search in the local database. Please use it to augment your answer and be more precise: 
             
@@ -169,16 +121,15 @@ if check_password():
             Don't put the same link twice in a response!
             
             If the user is not asking for any specific information, you shall always provide him with the link to all other articles on the Hult Tech Support Page: https://ef-hult.freshservice.com/support/solutions/50000010423
-
             """
         
         st.session_state.messages.append({"role": "user", "content": prompt})
         
         # Display the user's message in the chat interface
-        with st.chat_message("user", avatar=USER_AVATAR):
+        with st.chat_message("user", avatar="👩‍💻"):
             st.markdown(prompt)
 
-        # Step 4: Generate the response using the augmented prompt and chat history
+        # Generate the response using the augmented prompt and chat history
         with st.chat_message("assistant", avatar=BOT_AVATAR):
             message_placeholder = st.empty()
             full_response = ""
@@ -191,10 +142,5 @@ if check_password():
                 message_placeholder.markdown(full_response + "|")
             message_placeholder.markdown(full_response)
         
-        # Step 5: Append the assistant's response to the chat history
+        # Append the assistant's response to the chat history
         st.session_state.messages.append({"role": "assistant", "content": full_response})
-
-    # Step 6: Save chat history after each interaction
-    save_chat_history(st.session_state.messages)
-
-    # Save chat history after each interaction
